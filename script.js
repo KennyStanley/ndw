0e42ab8e41892b779894aca9a89398de707817c0
Kenny Stanley <kpstanleyjr@gmail.com>
2023-05-24 09:51:15 -0500

    Removed unused import

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import { CSG } from './utils/CSGMesh.js'
 
 /**
  * from dom and window and other objects
